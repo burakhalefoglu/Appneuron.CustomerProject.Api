de986eb41990bd7d29cb458ef264b42582458c73
burak <burak@DESKTOP-MUSTQJ0.local>
2022-02-11 16:27:31 +0300

    Update Cassandracontext.cs

---
+++ DataAccess/Concrete/Cassandra/Contexts/Cassandracontext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DataAccess.Concrete.Cassandra.Contexts
+{
+    public class Cassandracontext: CassandraContextBase
+    {
+        public Cassandracontext(IConfiguration configuration) : base(configuration)
+        {
+        }
+        
+    }
+}
