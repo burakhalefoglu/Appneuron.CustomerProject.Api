baeb16094747dc9c8e70b4756d2e1f0b36f6f560
burakhalefoglu <burakhalefoglu@gmail.com>
2022-03-08 10:34:28 +0300

    Update HealthChecksController.cs

---
+++ WebAPI/Controllers/HealthChecksController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Core.Utilities.Results;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using IResult = Core.Utilities.Results.IResult;
+
+namespace WebAPI.Controllers
+{
+    /// <summary>
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthChecksController : BaseApiController
+    {
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [Consumes("application/json")]
+        [Produces("application/json", "text/plain")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IResult))]
+        [HttpGet("get")]
+        public Task<IActionResult> CheckHealth()
+        {
+            return Task.FromResult<IActionResult>(Ok(new SuccessResult()));
+        }
+    }
+}
