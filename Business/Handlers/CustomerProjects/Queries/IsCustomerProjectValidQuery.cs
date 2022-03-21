52379902f2c89cda75e1f737ad3e55d4b3234053
burakhalefoglu <burakhalefoglu@gmail.com>
2022-03-21 14:37:22 +0300

    Update IsCustomerProjectValidQuery.cs and CustomerProjectsController.cs

---
+++ Business/Handlers/CustomerProjects/Queries/IsCustomerProjectValidQuery.cs
@@ -0,0 +1,48 @@
+﻿using Business.BusinessAspects;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Logging;
+using Core.Aspects.Autofac.Performance;
+using Core.CrossCuttingConcerns.Logging.Serilog.Loggers;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace Business.Handlers.CustomerProjects.Queries;
+
+public class IsCustomerProjectValidQuery : IRequest<IDataResult<bool>>
+{
+    public long ProjectId { get; set; }
+    
+    public class IsCustomerProjectValidQueryHandler : IRequestHandler<IsCustomerProjectValidQuery,
+        IDataResult<bool>>
+    {
+        private readonly ICustomerProjectRepository _customerProjectRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IMediator _mediator;
+
+        public IsCustomerProjectValidQueryHandler(ICustomerProjectRepository customerProjectRepository,
+            IMediator mediator, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _customerProjectRepository = customerProjectRepository;
+            _mediator = mediator;
+        }
+
+        [PerformanceAspect(5)]
+        [CacheAspect(10)]
+        [LogAspect(typeof(ConsoleLogger))]
+        [SecuredOperation(Priority = 1)]
+        public async Task<IDataResult<bool>> Handle(IsCustomerProjectValidQuery request,
+            CancellationToken cancellationToken)
+        {
+            var userId = _httpContextAccessor.HttpContext?.User.Claims
+                .FirstOrDefault(x => x.Type.EndsWith("nameidentifier"))?.Value;
+                
+            return new SuccessDataResult<bool>(
+                _customerProjectRepository.GetListAsync().Result.ToList()
+                    .Any(p => p.CustomerId == Convert.ToInt64(userId) &&  p.Id == request.ProjectId));
+        }
+    }
+}
--- WebAPI/Controllers/CustomerProjectsController.cs
+++ WebAPI/Controllers/CustomerProjectsController.cs
@@ -32,6 +32,26 @@ namespace WebAPI.Controllers
         }
 
         /// <summary>
+        ///     List CustomerProjects
+        /// </summary>
+        /// <remarks>CustomerProjects</remarks>
+        /// <return>List CustomerProjects</return>
+        /// <response code="200"></response>
+        [Produces("application/json", "text/plain")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IDataResult<bool>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(IResult))]
+        [HttpGet("isValid")]
+        public async Task<IActionResult> IsProjectValid(long projectId)
+        {
+            var result = await Mediator.Send(new IsCustomerProjectValidQuery
+            {
+                ProjectId = projectId
+            });
+            if (result.Success) return Ok(result);
+            return BadRequest(result);
+        }
+
+        /// <summary>
         ///     It brings the details according to its id.
         /// </summary>
         /// <remarks>CustomerProjects</remarks>
@@ -41,7 +61,7 @@ namespace WebAPI.Controllers
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IDataResult<CustomerProject>))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(IResult))]
         [HttpGet]
-        public async Task<IActionResult> GetByProjectKey(string name)
+        public async Task<IActionResult> Get(string name)
         {
             var result = await Mediator.Send(new GetCustomerProjectQuery {Name = name});
             if (result.Success) return Ok(result);
